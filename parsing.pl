db8d4ccddeb023a42566ad677b6da2fb5fdba735
Dominic Humphries <dominich@broadbean.com>
2016-09-19 15:15:28 +0100

    Prompt and start thinking about eval...

--- parsing.pl
+++ parsing.pl
@@ -10,9 +10,19 @@ use feature 'say';
 say "Plisp - Lisp in Perl";
 
 # REPL me up!
-while (<>) {
-    chomp;
-    my $line = $_;
+prompt();
 
-    say "Your input was: '$line'...";
+sub prompt {
+    print "\nPlisp> ";
+
+    my $input = <>;
+    exit unless $input;
+    chomp $input;
+    say input_eval($input);
+    prompt();
+}
+
+sub input_eval {
+    my $line = shift;
+    $line;
 }
