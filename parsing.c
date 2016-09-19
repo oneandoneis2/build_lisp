75dfa5336d77307a09306d8154d8cf79cf8d4dae
Dominic Humphries <dominich@broadbean.com>
2016-09-19 11:40:19 +0100

    Parsing enabled!
    cc -std=c99 -Wall parsing.c mpc.c -ledit -lm -o parsing.o

--- parsing.c
+++ parsing.c
@@ -27,6 +27,22 @@ void add_history(char* unused) {}
 #endif
 
 int main(int argc, char** argv) {
+    // Create some parsers
+    mpc_parser_t* Number    = mpc_new("number");
+    mpc_parser_t* Operator  = mpc_new("operator");
+    mpc_parser_t* Expr      = mpc_new("expr");
+    mpc_parser_t* Lispy     = mpc_new("lispy");
+
+    // Define them
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "                                                           \
+            number      : /-?[0-9]+/ ;                              \
+            operator    : '+' | '-' | '*' | '/' ;                   \
+            expr        : <number> | '(' <operator> <expr>+ ')' ;   \
+            lispy       : /^/ <operator> <expr>+ /$/ ;              \
+        ",
+        Number, Operator, Expr, Lispy);
+
     puts("Lispy version 0.0.0.0.1");
     puts("Press Ctrl+c to Exit");
 
@@ -37,11 +53,23 @@ int main(int argc, char** argv) {
         // Save to history
         add_history(input);
 
-        // Echo
-        printf("You said: %s\n", input);
+        // Attempt to parse input
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            // Success! Print AST
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            // Failure :( Print the error
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         // Free up input
         free(input);
     }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+
     return 0;
 }
