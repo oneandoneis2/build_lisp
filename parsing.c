586328c289b8feddae9d3d58522e4350716c1aaf
Dominic Humphries <dominich@broadbean.com>
2016-09-22 16:01:52 +0100

    Update to handle errors

--- parsing.c
+++ parsing.c
@@ -26,36 +26,100 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
-long apply(long x, char* op, long y) {
-    if (strcmp(op, "+") == 0) { return x + y; }
-    if (strcmp(op, "-") == 0) { return x - y; }
-    if (strcmp(op, "*") == 0) { return x * y; }
-    if (strcmp(op, "/") == 0) { return x / y; }
-    return 0;
+/* Declare New Lisp Value Struct */
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+/* Create Enumeration of Possible lval Types */
+enum { LVAL_NUM, LVAL_ERR };
+
+/* Create Enumeration of Possible Error Types */
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+/* Create a new number type lval */
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
 }
 
-long eval(mpc_ast_t* t) {
-    // Numbers are self-evaluating
-    if (strstr(t->tag, "number")) {
-        return atoi(t->contents);
-    }
+/* Create a new error type lval */
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
 
-    // Must be an expression, then.
-    // Operator is always the second element
-    char* op = t->children[1]->contents;
+/* Print an "lval" */
+void lval_print(lval v) {
+  switch (v.type) {
+    /* In the case the type is a number print it */
+    /* Then 'break' out of the switch. */
+    case LVAL_NUM: printf("%li", v.num); break;
 
-    // Eval the first argument to the operator
-    // (there will always be at least one)
-    long x = eval(t->children[2]);
+    /* In the case the type is an error */
+    case LVAL_ERR:
+      /* Check what type of error it is and print it */
+      if (v.err == LERR_DIV_ZERO) {
+        printf("Error: Division By Zero!");
+      }
+      if (v.err == LERR_BAD_OP)   {
+        printf("Error: Invalid Operator!");
+      }
+      if (v.err == LERR_BAD_NUM)  {
+        printf("Error: Invalid Number!");
+      }
+    break;
+  }
+}
 
-    // Eval any remaining args
-    int i = 3;
-    while (strstr(t->children[i]->tag, "expr")) {
-        x = apply(x, op, eval(t->children[i]));
-        i++;
-    }
+/* Print an "lval" followed by a newline */
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
 
-    return x;
+lval apply(lval x, char* op, lval y) {
+
+  /* If either value is an error return it */
+  if (x.type == LVAL_ERR) { return x; }
+  if (y.type == LVAL_ERR) { return y; }
+
+  /* Otherwise do maths on the number values */
+  if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+  if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+  if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+  if (strcmp(op, "/") == 0) {
+    /* If second operand is zero return error */
+    return y.num == 0
+      ? lval_err(LERR_DIV_ZERO)
+      : lval_num(x.num / y.num);
+  }
+
+  return lval_err(LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t* t) {
+
+  if (strstr(t->tag, "number")) {
+    /* Check if there is some error in conversion */
+    errno = 0;
+    long x = strtol(t->contents, NULL, 10);
+    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+  }
+
+  char* op = t->children[1]->contents;
+  lval x = eval(t->children[2]);
+
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = apply(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
 }
 
 int main(int argc, char** argv) {
@@ -89,8 +153,8 @@ int main(int argc, char** argv) {
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
             // Success! Eval & print answer
-            long result = eval(r.output);
-            printf("%li\n", result);
+            lval result = eval(r.output);
+            lval_println(result);
             mpc_ast_delete(r.output);
         } else {
             // Failure :( Print the error
